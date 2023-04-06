6ca408d307ed441958f3df88e40a5c9ed8f197a8
TheNickHub <shenn18@terpmail.umd.edu>
2023-04-05 23:15:49 -0400

    Lab 7 finished and starting lab 8

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -34,7 +34,6 @@ function cutRestaurantList(list) {
 async function mainEvent() {
   // the async keyword means we can make API requests
   const mainForm = document.querySelector(".main_form");
-  const filterDataButton = document.querySelector("#filter");
   const loadDataButton = document.querySelector("#data_load");
   const generateListButton = document.querySelector("#generate");
   const textField = document.querySelector("#resto");
@@ -43,7 +42,12 @@ async function mainEvent() {
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
 
-  let storedList = [];
+  const storedData = localStorage.getItem("storedData");
+  const parsedData = JSON.parse(storedData);
+  if (parsedData.length > 0) {
+    generateListButton.classList.remove("hidden");
+  }
+
   let currentList = [];
 
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -58,31 +62,16 @@ async function mainEvent() {
     );
 
     // This changes the response from the GET into data we can use - an "object"
-    storedList = await results.json();
-    if (storedList.length > 0) {
-      generateListButton.classList.remove("hidden");
-    }
+    const storedList = await results.json();
+    localStorage.setItem("storedData", JSON.stringify(storedList));
 
     loadAnimation.style.display = "none";
-    console.table(storedList);
-  });
-
-  filterDataButton.addEventListener("click", (event) => {
-    console.log("clicked FilterButton");
-
-    const formData = new FormData(mainForm);
-    const formProps = Object.fromEntries(formData);
-
-    console.log(formProps);
-
-    const newList = filterList(currentList, formProps.resto);
-    console.log(newList);
-    injectHTML(newList);
+    // console.table(storedList);
   });
 
   generateListButton.addEventListener("click", (event) => {
     console.log("generate new list");
-    currentList = cutRestaurantList(storedList);
+    currentList = cutRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
   });
